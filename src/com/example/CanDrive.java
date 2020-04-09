772a854f1ed03a8ffcdd229d9080309c9225b08f
tomek <tomeksochacki@wp.pl>
2020-04-09 11:45:03 +0200

    Initialize repository

---
+++ src/com/example/CanDrive.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public interface CanDrive {
+    public void start();
+
+    public void stop();
+
+    public void turnLeft();
+
+    public void turnRight();
+
+    public void speedMax();
+
+    public void acceleration();
+}
